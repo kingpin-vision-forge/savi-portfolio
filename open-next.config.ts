bbb66a3c2c11626d3f7aabe865142760638c37c3
Niyaz Ahamad Herkal <niyaz47nhh@gmail.com>
2026-04-19 16:56:43 +0530

    refactor: remove KV cache configuration from wrangler and open-next settings

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
-import kvTagCache from "@opennextjs/cloudflare/overrides/tag-cache/kv-next-tag-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
-  tagCache: kvTagCache,
-});
+export default defineCloudflareConfig();
